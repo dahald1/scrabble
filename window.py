da97e8e38aea619c2a67417e75c8c099e5a18b8b
Colin Butera <ccmjbutera@gmail.com>
2025-03-05 12:58:04 -0500

    added text to special spaces

--- window.py
+++ window.py
@@ -15,14 +15,15 @@ TILE_SIZE = (WINDOW_WIDTH - PADDING) // GRID_SIZE
 # Special Tile Constants (ZERO INDEX)
 TRIPLE_WORD = [(0, 0), (0, 7), (0, 14), (7, 0), (7, 14), (14, 0), (14, 7), (14, 14)]
 TRIPLE_LETTER = [(1, 5), (1, 9), (5, 1), (5, 5), (5, 9), (5, 13),
-                       (9, 1), (9, 5), (9, 9), (9, 13), (13, 5), (13, 9)]
+                 (9, 1), (9, 5), (9, 9), (9, 13), (13, 5), (13, 9)]
 DOUBLE_WORD = [(1, 1), (2, 2), (3, 3), (4, 4), (10, 10), (11, 11), (12, 12), (13, 13),
-                     (1, 13), (2, 12), (3, 11), (4, 10), (10, 4), (11, 3), (12, 2), (13, 1)]
+               (1, 13), (2, 12), (3, 11), (4, 10), (10, 4), (11, 3), (12, 2), (13, 1)]
 DOUBLE_LETTER = [(0, 3), (0, 11), (2, 6), (2, 8), (3, 0), (3, 7), (3, 14),
-                       (6, 2), (6, 6), (6, 8), (6, 12), (7, 3), (7, 11), (8, 2),
-                       (8, 6), (8, 8), (8, 12), (11, 0), (11, 7), (11, 14),
-                       (12, 6), (12, 8), (14, 3), (14, 11)]
+                 (6, 2), (6, 6), (6, 8), (6, 12), (7, 3), (7, 11), (8, 2),
+                 (8, 6), (8, 8), (8, 12), (11, 0), (11, 7), (11, 14),
+                 (12, 6), (12, 8), (14, 3), (14, 11)]
 CENTER = [(7, 7)]
+
 
 class GameView(arcade.Window):
     def __init__(self):
@@ -39,24 +40,35 @@ class GameView(arcade.Window):
 
         for row in range(GRID_SIZE):
             for col in range(GRID_SIZE):
+                text = []
                 x = col * TILE_SIZE + PADDING // 2
                 y = row * TILE_SIZE + PADDING // 2
 
                 if (row, col) in TRIPLE_WORD:
                     color = arcade.color.LIGHT_RED_OCHRE
+                    text = ["TRIPLE", "WORD", "SCORE"]
                 elif (row, col) in TRIPLE_LETTER:
                     color = arcade.color.BALL_BLUE
+                    text = ["TRIPLE", "LETTER", "SCORE"]
                 elif (row, col) in DOUBLE_WORD:
                     color = arcade.color.LIGHT_PINK
+                    text = ["DOUBLE", "WORD", "SCORE"]
                 elif (row, col) in DOUBLE_LETTER:
                     color = arcade.color.LIGHT_BLUE
+                    text = ["DOUBLE", "LETTER", "SCORE"]
                 elif (row, col) in CENTER:
                     color = arcade.color.LIGHT_GREEN
                 else:
                     color = arcade.color.BEIGE
+                    text = []
 
                 arcade.draw_lbwh_rectangle_filled(x, y, TILE_SIZE, TILE_SIZE, color)
                 arcade.draw_lbwh_rectangle_outline(x, y, TILE_SIZE, TILE_SIZE, arcade.color.NAVY_BLUE)
+                x = x + 22
+                start_y = y + 5
+                for i, word in enumerate(text):
+                    y = start_y - (i * 10)
+                    arcade.draw_text(word, x, y + 25, arcade.color.BLACK, 5, anchor_x="center")
 
 
     def on_update(self, delta_time):
@@ -109,7 +121,6 @@ def main():
     window.setup()
 
     arcade.run()
-
 
 
 if __name__ == "__main__":
