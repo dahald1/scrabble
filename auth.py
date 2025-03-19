1ba26da1be92f47401e01e2fd35dae0c48f454e3
jmathies <jakob.mathieson@uvm.edu>
2025-03-19 15:34:18 -0400

    maximized pylint rating and renamed serviceAccount file

--- auth.py
+++ auth.py
@@ -1,16 +1,18 @@
+"""Auth file used to get reference to Firestore database"""
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 
 def auth():
-    # returns reference to database
+    """returns reference to database"""
     # NOTE: do not push the scrabble_SDK.json to the GitHub repository!
     # Leave it one directory above the project folder!
-    cred = credentials.Certificate('../scrabble_SDK.json')
-    app = firebase_admin.initialize_app(cred)
-    db = firestore.client()
-    return db
+    cred = credentials.Certificate('../serviceAccount.json')
+    _ = firebase_admin.initialize_app(cred)
+    database = firestore.client()
+    return database
 
 
 if __name__ == "__main__":
--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+"""Contains functions for saving and loading game data from database"""
 from auth import auth
 
 SAVE_DOC_NAME = "save1"
@@ -6,19 +7,26 @@ db = auth()
 
 
 class ScrabbleGame:
-    def __init__(self, board=[], turn=0, player_score=0, ai_score=0):
+    """Class representing Scrabble game data"""
+
+    def __init__(self, board=None, turn=0, player_score=0, ai_score=0):
+        if board is None:
+            board = []
+
         self.board = board
         self.turn = turn
         self.player_score = player_score
         self.ai_score = ai_score
 
-    def from_dict(self, dict):
-        self.board = dict["board"]
-        self.turn = dict["turn"]
-        self.player_score = dict["player_score"]
-        self.ai_score = dict["ai_score"]
+    def from_dict(self, dictionary):
+        """Sets fields to passed dictionary values"""
+        self.board = dictionary["board"]
+        self.turn = dictionary["turn"]
+        self.player_score = dictionary["player_score"]
+        self.ai_score = dictionary["ai_score"]
 
     def to_dict(self):
+        """Returns game data as a dictionary"""
         return {
             "board": self.board,
             "turn": self.turn,
@@ -27,35 +35,40 @@ class ScrabbleGame:
         }
 
     def print(self):
+        """Prints game data for debugging"""
         print(f"Board: {self.board}\
         Turn: {self.turn}\
         Player Score: {self.player_score}\
         AI Score: {self.ai_score}\
         ")
 
-def save_game(game: ScrabbleGame, save_name):
+def save_game(game_to_save: ScrabbleGame, save_name):
+    """saves game to firestore document save_name"""
     doc_ref = db.collection("saves").document(save_name)
-    doc_ref.set(game.to_dict())
+    doc_ref.set(game_to_save.to_dict())
 
 def load_game(save_name) -> ScrabbleGame | None:
+    """loads game from firestore document save_name"""
     doc = db.collection("saves").document(save_name).get()
 
     if not doc.exists:
         print("No document found for '{save_name}'")
         return None
 
-    game = ScrabbleGame()
-    game.from_dict(doc.to_dict())
-    
-    return game
+    loaded_game = ScrabbleGame()
+    loaded_game.from_dict(doc.to_dict())
+
+    return loaded_game
 
 
 if __name__ == "__main__":
     # placeholder data structure
     data = {
-        "board": [{"x": 2, "y": 3, "letter": "b"}, {"x": 3, "y": 3, "letter": "a"}, {"x": 4, "y": 3, "letter": "d"}],
-        "turn": 3, 
-        "player_score": 6, 
+        "board": [{"x": 2, "y": 3, "letter": "b"}, \
+                  {"x": 3, "y": 3, "letter": "a"}, \
+                  {"x": 4, "y": 3, "letter": "d"}],
+        "turn": 3,
+        "player_score": 6,
         "ai_score": 0
     }
 
